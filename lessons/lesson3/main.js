2273e3aa84c2368a64534ff2b9b8f9a2509a628e
Gergi Maksyma-Fernanda <gergimaksima@gmail.com>
2023-02-08 17:16:04 +0100

    lesson2

--- lessons/lesson3/main.js
+++ lessons/lesson3/main.js
@@ -1,16 +1,29 @@
 // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
 for (let i = 0; i < 10; i++) {
-    document.write('<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, ad aliquam aperiam aspernatur distinctio eum fuga impedit in iusto laboriosam nihil odit perferendis porro quam quis quos recusandae rerum, sapiente!</div>');
+    document.write(`<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, ad aliquam aperiam aspernatur distinctio eum fuga impedit in iusto laboriosam nihil odit perferendis porro quam quis quos recusandae rerum, sapiente!</div>`);
 }
+;
 // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+for (let i = 0; i < 10; i++) {
+    document.write(`<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, ad aliquam aperiam aspernatur distinctio eum fuga impedit in iusto laboriosam nihil odit perferendis porro quam quis quos recusandae rerum, sapiente! ${i}</div>`);
+}
+;
 // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
 let i = 0;
 while (i < 20) {
-    document.write('<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, quasi.</h1>');
+    document.write(`<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, quasi.</h1>`);
     i++;
 }
+;
+
 // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
-//
+let o = 0;
+while (o < 20) {
+    document.write(`<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, quasi.${o}</h1>`);
+    o++;
+}
+;
+
 // - Використовуючи данні з масиву, за допомоги document.write та циклу
 // побудувати структуру по шаблону
 // Масив:
@@ -37,6 +50,7 @@ for (let item of listOfItems) {
     </ul>
     </div>`)
 }
+;
 // -----------------------------------------------
 //
 //     Використовуючи данні з масиву, за допомоги document.write та циклу
@@ -73,8 +87,34 @@ for (let item of listOfItems) {
 // </div>
 // Замість TITLE PRICE IMAGE - підставити відповідні поля з об'єкту
 //
-//
-//
+let products = [
+    {
+        title: 'milk',
+        price: 22,
+        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+    },
+    {
+        title: 'juice',
+        price: 27,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+    },
+    {
+        title: 'tomato',
+        price: 47,
+        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+    },
+    {
+        title: 'tea',
+        price: 15,
+        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+    },];
+for (let product of products) {
+    document.write(`<div class="product-card">
+     <h3 class="product-title">${product.title}. Price - ${product.price}</h3>
+     <img src="${product.image}" alt="" class="product-image">
+ </div>`);
+
+}
 // --------------------
 //     є масив
 // let users = [
@@ -94,3 +134,31 @@ for (let item of listOfItems) {
 //     - користувачів зі статусом true
 // - користувачів зі статусом false
 // - користувачів які старші за 30 років
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+for (let user of users) {
+    if (user.status === true) {
+        console.log(user);
+    }
+}
+for (let user of users) {
+    if (user.status === false) {
+        console.log(user);
+    }
+}
+for (let user of users) {
+    if (user.age > 30) {
+        console.log(user);
+    }
+}
